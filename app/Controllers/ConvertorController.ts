22386ea94dc93d6120c10e52d9869701c916f294
ronald <ronald.barloy.it@gmail.com>
2022-06-03 20:36:44 +0200

    j'ai changé se connecter par se cionnectarrrrrrer   nike ta merer

--- app/Controllers/ConvertorController.ts
+++ app/Controllers/ConvertorController.ts
@@ -2,15 +2,7 @@
 export default class ConvertorController {
 
     convertBytesToGigabytes( bytes: number ) : number {
-        
-        let gigabytes: number
 
-        if( bytes < 1000000000){
-            gigabytes = Math.round(bytes / 1e9 * 100 ) / 100
-        } else {
-            gigabytes = Math.round(bytes / 1e9 * 10 ) / 10
-        }
-
-        return gigabytes
+        return bytes < 1000000000 ? Math.round(bytes / 1e9 * 100 ) / 100 : Math.round(bytes / 1e9 * 10 ) / 10
     }
 }
